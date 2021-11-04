5ac9f4f1d3dd7e9615ce7f4d48c83fbe3320fefa
Bryan Calisto <bryancalisto@yahoo.es>
2021-11-03 20:18:11 -0500

    Refactor: Changed layout to tabView with bottom navigation bar

--- graphics/lib/main.dart
+++ graphics/lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:graphics/drawing_board/drawing_page.dart';
 import 'package:graphics/transform_widgets/magic_ball.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentViewIndex = 0;
+  final List<Widget> _tabsContent = const [
+    Center(child: MagicBall()),
+    Center(child: DrawingPage()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,22 +26,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: DefaultTabController(
-            length: 2,
-            child: TabBarView(
-              children: [
-                Center(
-                  child: MagicBall(),
-                ),
-                Center(
-                  child: Text('another thing'),
-                ),
+      home: DefaultTabController(
+        length: _tabsContent.length,
+        child: Scaffold(
+            bottomNavigationBar: BottomNavigationBar(
+              backgroundColor: Colors.black,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.transform, color: Colors.white), label: 'Transform'),
+                BottomNavigationBarItem(icon: Icon(Icons.art_track, color: Colors.white), label: 'Canvas'),
               ],
+              iconSize: 30,
+              onTap: (int index) => setState(() => _currentViewIndex = index),
+              currentIndex: _currentViewIndex,
+              selectedItemColor: Colors.grey,
+              unselectedItemColor: Colors.grey,
+              showUnselectedLabels: true,
+              showSelectedLabels: true,
             ),
-          ),
-        ),
+            body: _tabsContent[_currentViewIndex]),
       ),
     );
   }
