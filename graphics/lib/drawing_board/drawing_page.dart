5b90ba16eaa2c9ebaa97dc726ec0a6de3e59e6d0
Bryan Calisto <bryancalisto@yahoo.es>
2021-11-04 14:02:43 -0500

    Chore: Added undo functionality

--- graphics/lib/drawing_board/drawing_page.dart
+++ graphics/lib/drawing_board/drawing_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:graphics/drawing_board/drawer.dart';
 import 'package:graphics/drawing_board/drawn_path.dart';
 
-import 'learning.dart';
-
 class DrawingPage extends StatefulWidget {
   const DrawingPage({Key? key}) : super(key: key);
 
@@ -12,7 +10,11 @@ class DrawingPage extends StatefulWidget {
 }
 
 class _DrawingPageState extends State<DrawingPage> {
-  List<Offset> pathPoints = [];
+  late List<Offset> pathPoints;
+  List<DrawnPath> paths = [];
+  late DrawnPath currentPath;
+  double width = 2;
+  Color color = Colors.black;
 
   GestureDetector buildCurrentPath(BuildContext context) {
     return GestureDetector(
@@ -23,24 +25,51 @@ class _DrawingPageState extends State<DrawingPage> {
         color: Colors.transparent,
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: CustomPaint(painter: CustomDrawer(pathPoints: pathPoints)),
+        child: CustomPaint(painter: CustomDrawer(paths: paths)),
       ),
     );
   }
 
+  void undo() {
+    if (paths.isNotEmpty) {
+      paths.removeLast();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        buildCurrentPath(context),
-      ],
+    return SafeArea(
+      child: Stack(
+        children: [
+          buildCurrentPath(context),
+          Positioned(
+            right: 10,
+            top: 10,
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: undo,
+                  child: const Icon(Icons.undo, color: Colors.white),
+                  style: TextButton.styleFrom(
+                    shape: const CircleBorder(),
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.all(10),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
   void onPanStart(DragStartDetails details) {
     if (mounted) {
       final point = (context.findRenderObject() as RenderBox).globalToLocal(details.globalPosition);
-      pathPoints.clear();
+      pathPoints = <Offset>[];
+      currentPath = DrawnPath(pathPoints: pathPoints, width: width, color: color);
+      paths.add(currentPath);
       setState(() {
         pathPoints.add(point);
       });
@@ -50,15 +79,16 @@ class _DrawingPageState extends State<DrawingPage> {
   void onPanUpdate(DragUpdateDetails details) {
     if (mounted) {
       final point = (context.findRenderObject() as RenderBox).globalToLocal(details.globalPosition);
-      pathPoints.add(point);
-      setState(() {});
+      setState(() {
+        pathPoints.add(point);
+      });
     }
   }
 
   void onPanEnd(DragEndDetails details) {
     if (mounted) {
       print('finished drawing');
+      setState(() {});
     }
-    setState(() {});
   }
 }
